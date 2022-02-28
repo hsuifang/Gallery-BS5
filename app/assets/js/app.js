0a45856471d3600511623d8f9e422d1fce5ffc48
hsuifang <whalekiller03@gmail.com>
2022-02-28 22:06:13 +0800

    4. mobile/desktop - cart

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,1 +1,17 @@
 console.log("app");
+
+const cartBtn = document.getElementById("js-cart-btn");
+const cartField = document.getElementById("js-cart");
+
+cartBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (cartField.classList.contains("show")) {
+    cartField.classList.remove("show");
+    document.body.style.height = "auto";
+    document.body.style.overflow = "auto";
+  } else {
+    cartField.classList.add("show");
+    document.body.style.height = "100vh";
+    document.body.style.overflow = "hidden";
+  }
+});
